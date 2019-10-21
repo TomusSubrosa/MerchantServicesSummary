4c822ab8466eb052336ed09ebfef234868edf961
Martin Esani <martin.esani@segmatic.io>
2019-10-21 16:33:30 +0100

    adding null for orders to run.

--- source/OrdersRunner.cs
+++ source/OrdersRunner.cs
@@ -8,7 +8,7 @@ namespace ShoppingSamples.Content
         internal static void Main(string[] args)
         {
             var samples = new OrdersSample();
-            samples.startSamples(args);
+            samples.startSamples(args,null);
         }
     }
 }
